e0fe372eaabda23f2889d88c629762665da6e932
John Mihalic <2854333+mezz64@users.noreply.github.com>
2023-12-05 21:55:20 -0500

    Cleanup

--- components/ddp/ddp_basic_light_effect.cpp
+++ components/ddp/ddp_basic_light_effect.cpp
@@ -15,19 +15,11 @@ const std::string &DDPBasicLightEffect::get_name() { return LightEffect::get_nam
 
 void DDPBasicLightEffect::start() {
 
-  // backup gamma for restoring when effect ends
-  // this->gamma_backup_ = this->state_->get_gamma_correct();
-  // this->next_packet_will_be_first_ = true;
-
   LightEffect::start();
   DDPLightEffectBase::start();
 }
 
 void DDPBasicLightEffect::stop() {
-
-  // restore gamma.
-  // this->state_->set_gamma_correct(this->gamma_backup_);
-  // this->next_packet_will_be_first_ = true;
 
   DDPLightEffectBase::stop();
   LightEffect::stop();
@@ -43,17 +35,6 @@ void DDPBasicLightEffect::apply() {
     this->next_packet_will_be_first_ = true;
 
     auto call = this->state_->turn_on();
-
-    // call.set_color_mode_if_supported(this->state_->remote_values.get_color_mode());
-    // call.set_red_if_supported(this->state_->remote_values.get_red());
-    // call.set_green_if_supported(this->state_->remote_values.get_green());
-    // call.set_blue_if_supported(this->state_->remote_values.get_blue());
-    // call.set_brightness_if_supported(this->state_->remote_values.get_brightness());
-    // call.set_color_brightness_if_supported(this->state_->remote_values.get_color_brightness());
-
-    // call.set_white_if_supported(this->state_->remote_values.get_white());
-    // call.set_cold_white_if_supported(this->state_->remote_values.get_cold_white());
-    // call.set_warm_white_if_supported(this->state_->remote_values.get_warm_white());
 
     call.set_publish(false);
     call.set_save(false);
@@ -71,14 +52,6 @@ uint16_t DDPBasicLightEffect::process_(const uint8_t *payload, uint16_t size, ui
   // If there aren't 3 unused bytes, return 0 to indicate error.
   if ( size < (used + 3) ) { return 0; }
 
-  // disable gamma on first received packet, not just based on effect being enabled.
-  // that way home assistant light can still be used as normal when DDP packets are not
-  // being received but effect is still enabled.
-  // gamma will be enabled again when effect disabled or on timeout.
-  // if ( this->next_packet_will_be_first_ && this->disable_gamma_ ) {
-  //   this->state_->set_gamma_correct(0.0f);
-  // }
-
   this->next_packet_will_be_first_ = false;
   this->last_ddp_time_ms_ = millis();
 
@@ -88,46 +61,15 @@ uint16_t DDPBasicLightEffect::process_(const uint8_t *payload, uint16_t size, ui
   float green = (float)payload[used+1]/255.0f;
   float blue  = (float)payload[used+2]/255.0f;
 
-  // float multiplier = this->state_->remote_values.get_brightness();
-  // float max_val = 0;
-
-  // // for DDP_PACKET mode, find largest r,g, or b value in packet and scale that value to brightness
-  // if ( this->scaling_mode_ == DDP_SCALE_PACKET ) {
-  //   // find largest rgb value in packet
-  //   uint8_t packet_max = 0;
-  //   for ( int i = 10; i < size; i++ ) {
-  //     if ( payload[i] > packet_max ) { packet_max = payload[i]; }
-  //   }
-  //   max_val = (float)packet_max / 255.0f;
-  // }
-
-  // // DDP_PIXEL and DDP_STRIP are the same with bulbs since a "strip" is one pixel.
-  // if ( (this->scaling_mode_ == DDP_SCALE_STRIP) || (this->scaling_mode_ == DDP_SCALE_PIXEL)) {
-  //   if ( (red >= green) && (red >= blue ) ) { max_val = red;   }
-  //   else if             ( green >= blue )   { max_val = green; }
-  //   else                                    { max_val = blue;  }
-  // }
-
-  // // if we got a max_val, update multiplier
-  // if ( max_val != 0 ) { multiplier /= max_val; }
-
-  // // if we are in any scaling mode, multiply the pixel rgb values by the multiplier.
-  // if ( this->scaling_mode_ != DDP_NO_SCALING) {
-  //   red   *= multiplier;
-  //   green *= multiplier;
-  //   blue  *= multiplier;
-  // }
-
   ESP_LOGV(TAG, "Final RGB data for '%s->%s': (%02x,%02x,%02x)", this->state_->get_name().c_str(), this->get_name().c_str(), red, green, blue);
 
   // We can only turn on/off, so if we have white (255,255,255) we go on, anything less is off
   auto call = this->state_->turn_on();
 
-  if ( (red+green+blue) == 3 ) {
+  if ( (red + green + blue) == 3 ) {
     call.set_brightness(255.0f);
     call.set_state(true);
   } else {
-    // auto call = this->state_->turn_off(); // Can't do this, it kills the effect
     call.set_brightness(0.0f);
     call.set_state(true);
   }
@@ -137,26 +79,6 @@ uint16_t DDPBasicLightEffect::process_(const uint8_t *payload, uint16_t size, ui
   call.set_publish(false);
   call.set_save(false);
   call.perform();
-
-  // auto call = this->state_->turn_on();
-
-  // call.set_color_mode_if_supported(light::ColorMode::RGB_COLD_WARM_WHITE);
-  // call.set_color_mode_if_supported(light::ColorMode::RGB_COLOR_TEMPERATURE);
-  // call.set_color_mode_if_supported(light::ColorMode::RGB_WHITE);
-  // call.set_color_mode_if_supported(light::ColorMode::RGB);
-  // call.set_red_if_supported(red);
-  // call.set_green_if_supported(green);
-  // call.set_blue_if_supported(blue);
-  // call.set_brightness_if_supported(std::max(red, std::max(green, blue)) );
-  // call.set_color_brightness_if_supported(1.0f);
-
-  // // disable white channels
-  // call.set_white_if_supported(0.0f);
-  // call.set_cold_white_if_supported(0.0f);
-  // call.set_warm_white_if_supported(0.0f);
-
-  // call.set_transition_length_if_supported(0);
-  
 
   // manually calling loop otherwise we just go straight into processing the next DDP
   // packet without executing the light loop to display the just-processed packet.
